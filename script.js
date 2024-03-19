65ac4d6af36589e19ac5f3c9e22f48646e480fa4
Lucaxxsss <lucaxsgabrielslo@gmail.com>
2024-03-18 21:01:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function menu() {
+   let menu = document.querySelector('nav ul')
+   let menubtn = document.querySelector('nav .menu')
+
+   menu.classList.toggle('activated')
+   if(menu.classList.contains('activated')) {
+      menubtn.style.backdropFilter = 'blur(20px)'
+   } else {
+      menubtn.style.backdropFilter = 'blur(0px)'
+   }
+}
