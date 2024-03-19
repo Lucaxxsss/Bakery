4d7d0b4727d4c42baeecba9e06543b9bd6b56be4
Lucaxxsss <lucaxsgabrielslo@gmail.com>
2024-03-19 06:17:11 -0300

    improve UI/UX menu

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ function menu() {
    menu.classList.toggle('activated')
    if(menu.classList.contains('activated')) {
       menubtn.style.backdropFilter = 'blur(20px)'
+      menubtn.style.backgroundColor = 'rgba(0, 0, 0, 0.99)'
+      menubtn.style.transition = 'background-color 1s ease-in-out'
+      
       menubtn.classList.add('active')
    } else {
       menubtn.style.backdropFilter = 'blur(0px)'
+      menubtn.style.backgroundColor = 'transparent'
       menubtn.classList.remove('active')
    }
 }
