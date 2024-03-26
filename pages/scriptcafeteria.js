e0f7db100fda21fe81318f3bc4002340582523dc
Lucaxxsss <lucaxsgabrielslo@gmail.com>
2024-03-26 19:46:12 -0300

    popular now tab and responsivity

--- pages/scriptcafeteria.js
+++ pages/scriptcafeteria.js
@@ -9,3 +9,41 @@ function menuclick() {
       hamb.classList.remove('actv')
    }
 }
+
+let infoCoffee = document.getElementsByClassName('info-coffee')
+
+let infoCoffeeArray = Array.from(infoCoffee)
+
+console.log(infoCoffee)
+
+let info = [
+   {
+      nome: 'Espresso',
+      views: 29058
+   },
+   {
+      nome: 'Hazelnut Latte',
+      views: 43230
+   },
+   {
+      nome: 'Vanilla Latte',
+      views: 23203
+   }
+]
+
+function nameCoffee() {
+   const coffeInfo = (coffeeElement, index) => {
+      let coffeeTittle = document.createElement('h2')
+      coffeeTittle.innerHTML = info[index].nome
+
+      let coffeViews = document.createElement('span')
+      coffeViews.innerHTML = `${info[index].views}`
+
+      coffeeElement.appendChild(coffeeTittle)
+      coffeeElement.appendChild(coffeViews)
+   }
+
+   infoCoffeeArray.forEach(coffeInfo)
+}
+
+nameCoffee()
